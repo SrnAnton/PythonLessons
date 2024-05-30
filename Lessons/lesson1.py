8e2259cb3f29bc916bfb6c0f5a60f2b4b44aadc8
srn <srn.anton@gmail.com>
2024-05-30 13:30:36 +0300

    lesson 1. strings

---
+++ Lessons/lesson1.py
@@ -0,0 +1,11 @@
+import math
+
+example = "O4en'dlinnaya fraza"
+start = math.floor(len(example) / 2)
+
+print(example[0])
+print(example[-1])
+print(example[start:])
+print(example[::-1])
+print(example[1::2])
+
