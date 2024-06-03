21ca69b1ae37d0f8da2c7ae7ca431a687031908f
srn <srn.anton@gmail.com>
2024-06-03 15:44:50 +0300

    module 3. calculate all

---
+++ Lessons/module3.py
@@ -0,0 +1,42 @@
+def calculate_structure_sum(data):
+    sum_ = 0
+
+    if isinstance(data, int):
+        sum_ += data
+
+    elif isinstance(data, float):
+        sum_ += data
+
+    elif isinstance(data, str):
+        sum_ += len(data)
+
+    elif isinstance(data, list):
+        for i in data:
+            sum_ += calculate_structure_sum(i)
+
+    elif isinstance(data, tuple):
+        for i in data:
+            sum_ += calculate_structure_sum(i)
+
+    elif isinstance(data, set):
+        for i in data:
+            sum_ += calculate_structure_sum(i)
+
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            sum_ += calculate_structure_sum(key)
+            sum_ += calculate_structure_sum(value)
+
+    return sum_
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
