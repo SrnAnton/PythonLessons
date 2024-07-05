4da416ca066860a68424a0590ea55029142954d3
srn <srn.anton@gmail.com>
2024-07-05 11:49:04 +0300

    chapters 5-6

---
+++ Chapters/chapter5.py
@@ -0,0 +1,21 @@
+a = 1
+b = 2
+c = 3
+d = True
+e = False
+car = "bmw"
+toppings = ['mushrooms', 'onions', 'pineapple']
+# > < >= <= != == and or
+gde = 'mushrooms' in toppings
+ili = 'mushrooms' not in toppings
+
+if a == b:
+    print()
+elif a == c - b:
+    print()
+elif car != 'audi':
+    print()
+elif d:
+    print()
+else:
+    print()
