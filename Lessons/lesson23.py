dbf9decbfba21faa0193d91cf105b0194c0e766d
srn <srn.anton@gmail.com>
2024-07-05 13:26:40 +0300

    Lesson 23

---
+++ Lessons/lesson23.py
@@ -0,0 +1,56 @@
+# Цель: Закрепить знания о позиционировании в файле, использовав метод tell() файлового объекта. Написать усовершенствованную функцию записи.
+#
+# Задача "Записать и запомнить":
+# Создайте функцию custom_write(file_name, strings), которая принимает аргументы file_name - название файла для записи, strings - список строк для записи.
+# Функция должна:
+#
+#     Записывать в файл file_name все строки из списка strings, каждая на новой строке.
+#     Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>), а значением - записываемая строка. Для получения номера байта начала строки используйте метод tell() перед записью.
+#
+# Пример полученного словаря:
+# {(1, 0): 'Text for tell.', (2, 16): 'Используйте кодировку utf-8.'}
+# Где:
+# 1, 2 - номера записанных строк.
+# 0, 16 - номера байт, на которых началась запись строк.
+# 'Text for tell.', 'Используйте кодировку utf-8.' - сами строки.
+#
+# Пример результата выполнения программы:
+# Пример выполняемого кода:
+# info = [
+#     'Text for tell.',
+#     'Используйте кодировку utf-8.',
+#     'Because there are 2 languages!',
+#     'Спасибо!'
+#     ]
+#
+# result = custom_write('test.txt', info)
+# for elem in result.items():
+#   print(elem)
+#
+# Вывод на консоль:
+# ((1, 0), 'Text for tell.')
+# ((2, 16), 'Используйте кодировку utf-8.')
+# ((3, 66), 'Because there are 2 languages!')
+# ((4, 98), 'Спасибо!')
+
+def custom_write(file_name, strings):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        strings_positions = {}
+        for i, string in enumerate(strings, start=1):
+            position = file.tell()
+            file.write(string + '\n\r')
+            strings_positions[(i, position)] = string
+    return strings_positions
+
+
+# Пример использования функции
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for key, value in result.items():
+    print(key, value)
